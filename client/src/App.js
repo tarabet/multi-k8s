29e5c4ac09385f1b8a66935617c9ce97cbf7ce35
Alexander Taranenko <alexander.taranenko@matmatch.com>
2020-05-05 16:16:41 +0200

    Removed falthy symbol

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <p>CHANGED PAGE></p>
+          <p>CHANGED PAGE!</p>
           <Link to="/">Home</Link>
           <Link to="/other-page">Other Page</Link>
           <div>
